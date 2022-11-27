dc5d25a1bfbc60ef67ffec30e49d164158a9c655
Relate13 <201220114@smail.nju.edu.cn>
2022-11-27 16:47:48 +0800

    Implemented Main UI

---
+++ SE-Project/Assets/FileBrowsePanel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using AnotherFileBrowser.Windows;
+public class FileBrowsePanel : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    [SerializeField]
+    private string FileFilter;
+    // Start is called before the first frame update
+    public void SelectFile()
+    {
+        var bp = new BrowserProperties();
+        bp.filter = FileFilter;
+        bp.filterIndex = 0;
+        new FileBrowser().OpenFileBrowser(bp, path =>
+        {
+            inputField.text= path;
+            Debug.LogWarning(path);
+        });
+    }
+}
